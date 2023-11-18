cbb4d81b16ff8ca432e5e3fb2e6e915e1a2fc18f
ddhere <devanshdubey002@gmail.com>
2023-11-19 03:01:10 +0530

    131

--- public/script.js
+++ public/script.js
@@ -2,7 +2,8 @@ let last7DaysData = [];
 let last30DaysData = [];
 let betweenDaysData = [];
 let events = [];
-
+const commName = {};
+  const eventNames = {};
 document.addEventListener("DOMContentLoaded", async () => {
     // Global variables to store fetched data
     
@@ -20,15 +21,18 @@ document.addEventListener("DOMContentLoaded", async () => {
   
     // Fetch data for the last 7 days and store it in the global variable
     await fetchDataAndStoreInGlobalVariable("/api/last-7-days", last7DaysData);
-    await fetchDataAndStoreInGlobalVariable("/api/last-30-days", last30DaysData);
+    // await fetchDataAndStoreInGlobalVariable("/api/last-30-days", last30DaysData);
 
-  
+    
+    
+    // console.log("Last 30 Days Data:", last30DaysData);
+    getEventsData();
     console.log("All data fetch operations completed");
     
     // You can now access the data in the global variables as needed
+    last7DaysData = last7DaysData[0];
+    updateDataFromPostGres(last7DaysData);
     console.log("Last 7 Days Data:", last7DaysData);
-    console.log("Last 30 Days Data:", last30DaysData);
-    
 
   });
   
@@ -47,11 +51,14 @@ form.addEventListener("submit", async (event) => {
     betweenDaysData = data
 
     // Handle the fetched data (e.g., display it on the web page)
-    console.log(data);
     
+    console.log(data);
   } catch (error) {
     console.error("Error fetching data: ", error);
   }
+
+  updateDataFromPostGres(betweenDaysData)
+
 });
 
 async function fetchEvents() {
@@ -69,3 +76,36 @@ async function fetchEvents() {
 }
 
 fetchEvents();
+
+function createEventsDataDict(events) {
+  // Initialize dictionaries
+  
+
+  // Create commName dictionary
+  events.forEach(event => {
+    commName[event._id]=event.name;
+  });
+
+  // Create eventNames dictionary
+  events.forEach(event => {
+    event.events.forEach(innerEvent => {
+      eventNames[innerEvent.id]=innerEvent.name;
+    });
+  });
+
+  return { commName, eventNames };
+}
+function getEventsData(){
+  const { commName, eventNames } = createEventsDataDict(events);
+
+console.log('commName:', commName);
+console.log('eventNames:', eventNames); // Convert eventNames to an array of keys
+}
+
+function updateDataFromPostGres(arr){
+  arr.forEach(row => {
+    row.comm_name=commName[row.event_id];
+    row.task_id = row.task_id.replace(/\s+/g, '');
+    row.task_name=eventNames[row.task_id.replace(/\s+/g, '')];
+})
+}
